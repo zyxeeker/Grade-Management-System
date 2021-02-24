64fccaf6c0302bbcdc2d5d9adb58f28b1446207f
zyxeeker <190474450@qq.com>
2021-02-24 22:36:08 +0800

    增加IP报文一部分解析(未完成)

--- http/http.cpp
+++ http/http.cpp
@@ -1,4 +1,131 @@
 //
-// Created by 19047 on 2021/2/22.
+// Created by ZYXeeker on 2021/2/24.
 //
+#include <strings.h>
+#include "http.h"
 
+namespace http {
+
+    // 从状态机
+    HTTP_PACKET::HTTP_CODE HTTP_STATE_MACHINE::LINE_STATE(std::string str) {
+//        switch (m_packet_state) {
+//
+//        }
+
+        if (!str.compare(0, 7, "Accept:", 0, 7)) {
+            std::cout << str << std::endl;
+        }
+
+
+    }
+
+    // 请求方式状态机
+    HTTP_PACKET::HTTP_CODE HTTP_STATE_MACHINE::METHOD_STATE(std::string str) {
+
+    }
+
+    // 报文参数状态机
+    HTTP_PACKET::HTTP_CODE HTTP_STATE_MACHINE::HEADER_STATE(std::string str) {
+        if (!str.compare(0, 7, "Accept:", 0, 7)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "Referer:", 0, 8)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "Accept-Language:", 0, 16)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "Accept-Encoding:", 0, 16)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "User-Agent:", 0, 11)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "content-length:", 0, 15)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "Host:", 0, 5)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "Connection:", 0, 11)) {
+            std::cout << str << std::endl;
+        }
+        if (!str.compare(0, 7, "Cookie:", 0, 7)) {
+            std::cout << str << std::endl;
+        }
+
+
+    }
+
+    // 报文内容状态机
+    HTTP_PACKET::HTTP_CODE HTTP_STATE_MACHINE::CONTENT_STATE(std::string str) {
+
+    }
+
+
+    // 主状态机
+    HTTP_PACKET::HTTP_CODE HTTP_STATE_MACHINE::MAIN_STATE(std::string str) {
+
+        switch (m_packet_state) {
+            case CHECK_METHOD: {
+                HTTP_STATE_MACHINE::METHOD_STATE(str);
+                break;
+            }
+            case CHECK_HEADER: {
+                HTTP_STATE_MACHINE::HEADER_STATE(str);
+                break;
+            }
+            case CHECK_CONTENT: {
+                HTTP_STATE_MACHINE::CONTENT_STATE(str);
+                break;
+            }
+
+
+        }
+    }
+
+    bool HTTP_STATE_MACHINE::put_packet_state(PACKET_STATE pState) {
+        m_packet_state = pState;
+    }
+
+
+    HTTP_STATE_MACHINE::PACKET_STATE HTTP_STATE_MACHINE::m_packet_state = CHECK_METHOD;
+
+
+    void Http::read_packet(char *packet) {
+        int i;
+        std::string str1 = packet;
+//        std::cout << str1 << std::endl;
+        int length = str1.length();
+//        std::cout << length;
+
+        // line
+        for (i = 0; i < length; ++i) {
+            if (str1[i] == '\r') {
+                m_line = str1.substr(0, i);
+                str1 = str1.substr(i + 2);
+                std::cout << m_line << std::endl;
+                HTTP_STATE_MACHINE::put_packet_state(HTTP_STATE_MACHINE::PACKET_STATE::CHECK_METHOD);
+                HTTP_STATE_MACHINE::MAIN_STATE(m_line);
+                break;
+            }
+        }
+
+        // header
+        int begin_index = 0;
+        for (i = 0; i < str1.length();) {
+            if (str1[i] == '\r') {
+                m_header = str1.substr(begin_index, i - begin_index);
+//                std::cout << m_header << std::endl;
+                begin_index = i + 2;
+                i = i + 2;
+                HTTP_STATE_MACHINE::put_packet_state(HTTP_STATE_MACHINE::PACKET_STATE::CHECK_HEADER);
+                HTTP_STATE_MACHINE::MAIN_STATE(m_header);
+            } else
+                i++;
+        }
+
+
+    }
+
+}
--- http/http.h
+++ http/http.h
@@ -1,17 +1,27 @@
 //
-// Created by 19047 on 2021/2/22.
+// Created by ZYXeeker on 2021/2/22.
 //
 
 #ifndef WEBSEVER_HTTP_H
 #define WEBSEVER_HTTP_H
 
-namespace http {
-    class Http {
-    public:
-        enum HttpCode {
+#include <iostream>
 
+namespace http {
+
+    class HTTP_PACKET {
+    public:
+        enum HTTP_CODE {
+            NO_REQUEST,
+            GET_REQUEST,
+            BAD_REQUEST,
+            NO_RESOURCE,
+            FORBIDDEN_REQUEST,
+            FILE_REQUEST,
+            INTERNAL_ERROR,
+            CLOSED_CONNECTION
         };
-        enum HttpMethod {
+        enum HTTP_METHOD {
             GET,
             POST,
             PUT,
@@ -22,6 +32,68 @@ namespace http {
             CONNECT,
             PATH
         };
+    };
+
+    class HTTP_STATE_MACHINE {
+    public:
+        // 主状态机(判断报文)
+        enum PACKET_STATE {
+            CHECK_METHOD,
+            CHECK_HEADER,
+            CHECK_CONTENT
+        };
+        // 从状态机(判断报文语句是否正确)
+        enum LINE_STATE {
+            LINE_OK,
+            LINE_ERROR,
+            LINE_EMPTY
+        };
+
+        static bool put_packet_state(PACKET_STATE pState);
+
+        static HTTP_PACKET::HTTP_CODE MAIN_STATE(std::string str);
+
+        static HTTP_PACKET::HTTP_CODE LINE_STATE(std::string str);
+
+        HTTP_PACKET::HTTP_CODE PACKET_BLOCK_STATE();
+
+        static HTTP_PACKET::HTTP_CODE METHOD_STATE(std::string str);
+
+        static HTTP_PACKET::HTTP_CODE HEADER_STATE(std::string str);
+
+        static HTTP_PACKET::HTTP_CODE CONTENT_STATE(std::string str);
+
+        static PACKET_STATE m_packet_state;
+    private:
+    };
+
+    class Http {
+    public:
+        void init();
+
+        // 读取报文
+        void read_packet(char *packet);
+
+        void get_line();
+
+        void get_header();
+
+        void get_content();
+
+    private:
+        // 解析报文请求行
+        HTTP_PACKET::HTTP_CODE parse_line(char *line);
+
+        // 解析报文报头
+        HTTP_PACKET::HTTP_CODE parse_header(char *header);
+
+        // 解析报文内容
+        HTTP_PACKET::HTTP_CODE parse_content(char *content);
+
+        std::string m_line;
+        std::string m_header;
+        std::string m_content;
+
 
     };
 
