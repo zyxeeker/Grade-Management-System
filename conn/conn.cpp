522209e32d5bd0254204fa470f98abde20087d12
zyxeeker <190474450@qq.com>
2021-03-03 13:19:35 +0800

    改进代码层次

--- conn/conn.cpp
+++ conn/conn.cpp
@@ -2,107 +2,95 @@
 // Created by ZYXeeker on 2021/2/27.
 //
 
-#include <netinet/in.h>
 #include <iostream>
-#include <arpa/inet.h>
 #include <cstring>
 #include <unistd.h>
 #include "conn.h"
-#include "../http/http.h"
 
 namespace http_conn {
-    void conn::init(int port) {
-        http::Http http_packet;
-
-        http_packet.init();
-
-        int listenfd, conn;
+    int conn::socket_init(int port) {
         struct sockaddr_in addr;
-        char buff[4096];
-        int len;
         int on = 1;
 
-        // epoll
-        struct epoll_event event;
-        struct epoll_event *wait_event;
-        int epoll_fd = epoll_create1(EPOLL_CLOEXEC);
-
-
         // socket
-        if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        if ((m_listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
             std::cout << "ERROR" << std::endl;
-        }
 
         // bind
         addr.sin_family = AF_INET;
         addr.sin_port = htons(port);
         addr.sin_addr.s_addr = INADDR_ANY;
 
+        //// socket参数
         // 允许端口复用
-        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+        setsockopt(m_listen_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-        if (bind(listenfd, (struct sockaddr *) &addr, sizeof(addr)) == -1)
+        if (bind(m_listen_fd, (struct sockaddr *) &addr, sizeof(addr)) == -1)
             std::cout << "BIND　ERROR" << std::endl;
 
         // listen
-        if (listen(listenfd, 5) == -1) {
+        if (listen(m_listen_fd, 5) == -1) {
             std::cout << "LISTEN　ERROR" << std::endl;
         }
-
-        // epoll
-        event.data.fd = listenfd;
-        event.events = EPOLLIN;
-        epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listenfd, &event);
+    }
 
 
+    int conn::epoll(int MAX_CONNECTIONS) {
+        m_epoll_fd = epoll_create1(EPOLL_CLOEXEC);
+        m_event.data.fd = m_listen_fd;
+        m_event.events = EPOLLIN;
+        epoll_ctl(m_epoll_fd, EPOLL_CTL_ADD, m_listen_fd, &m_event);
+        m_wait_event = new epoll_event[MAX_CONNECTIONS];
 
-        // accept
-        char clientIP[INET_ADDRSTRLEN] = "";
-        struct sockaddr_in clientAddr;
-        socklen_t clientAddrLen = sizeof(clientAddr);
+    }
 
-        int i = 0;
-
-        wait_event = new epoll_event[10];
-
+    bool conn::socket_start(int MAX_CONNECTIONS) {
         while (true) {
-            int event_count = epoll_wait(epoll_fd, wait_event, 10, -1);
+            int event_count = epoll_wait(m_epoll_fd, m_wait_event, MAX_CONNECTIONS, -1);
 
             for (int j = 0; j < event_count; ++j) {
-                if (wait_event[j].data.fd == listenfd) {
-                    event.events = EPOLLIN;
-                    event.data.fd = conn;
+                if (m_wait_event[j].data.fd == m_listen_fd) {
+                    m_event.events = EPOLLIN;
+                    m_event.data.fd = m_conn;
+                    epoll_ctl(m_epoll_fd, EPOLL_CTL_ADD, m_epoll_fd, &m_event);
 
-                    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, epoll_fd, &event);
+                    m_conn = accept(m_listen_fd, (struct sockaddr *) &m_clientAddr, &m_clientAddrLen);
                     std::cout << "...listening" << std::endl;
-                    conn = accept(listenfd, (struct sockaddr *) &clientAddr, &clientAddrLen);
-                    inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, INET_ADDRSTRLEN);
-                    std::cout << "...connect " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
+                    inet_ntop(AF_INET, &m_clientAddr.sin_addr, m_clientIP, INET_ADDRSTRLEN);
+                    std::cout << "...connect " << m_clientIP << ":" << ntohs(m_clientAddr.sin_port) << std::endl;
 
-                    memset(buff, 0, sizeof(buff));
-                    len = recv(conn, buff, sizeof(buff), 0);
+                    memset(m_buff, 0, sizeof(m_buff));
+                    m_recv_len = recv(m_conn, m_buff, sizeof(m_buff), 0);
 
-                    buff[len - 1] = '\0';
-                    if (strcmp(buff, "\0") == 0) {
-                        std::cout << "...disconnect " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
+                    m_buff[m_recv_len - 1] = '\0';
+                    if (strcmp(m_buff, "\0") == 0) {
+                        std::cout << "...disconnect " << m_clientIP << ":" << ntohs(m_clientAddr.sin_port) << std::endl;
                         break;
                     }
-
-//                    std::cout <<buff<<std::endl;
-
+//                    std::cout << buff << std::endl;
                     // 解析报头
-                    http_packet.parse_packet(buff);
+                    m_http_packet.parse_packet(m_buff);
                     // 返回
-                    std::string header = http_packet.do_request();
-                    std::cout << header << std::endl;
+                    m_header = m_http_packet.do_request();
 
-                    send(conn, header.c_str(), header.length(), 0);
+                    send(m_conn, m_header.c_str(), m_header.length(), 0);
                 }
-                epoll_ctl(epoll_fd, EPOLL_CTL_DEL, conn, NULL);
-                close(conn);
+                epoll_ctl(m_epoll_fd, EPOLL_CTL_DEL, m_conn, NULL);
+                close(m_conn);
             }
         }
-        delete[] wait_event;
-        close(listenfd);
+    }
+
+    void conn::init(int MAX_CONNECTIONS, int port) {
+        m_http_packet.init();
+
+        socket_init(port);
+        // epoll启动
+        epoll(MAX_CONNECTIONS);
+
+        socket_start(MAX_CONNECTIONS);
+
+        delete[] m_wait_event;
+        close(m_listen_fd);
     }
 }
--- conn/conn.h
+++ conn/conn.h
@@ -7,13 +7,36 @@
 
 #include <sys/socket.h>
 #include <sys/epoll.h>
+#include "../http/http.h"
+#include <arpa/inet.h>
 
 namespace http_conn {
     class conn {
     public:
-        void init(int port = 9006);
+        void init(int MAX_CONNECTIONS, int port);
 
+        int socket_init(int port);
 
+        int epoll(int MAX_CONNECTIONS);
+
+        bool socket_start(int MAX_CONNECTIONS);
+
+    private:
+        http::Http m_http_packet;
+
+        struct epoll_event m_event;
+        struct epoll_event *m_wait_event;
+
+        int m_listen_fd;
+        int m_epoll_fd;
+        int m_conn;
+        int m_recv_len;
+        std::string m_header;
+
+        char m_buff[4096];
+        char m_clientIP[INET_ADDRSTRLEN] = "";
+        struct sockaddr_in m_clientAddr;
+        socklen_t m_clientAddrLen = sizeof(m_clientAddr);
     };
 }
 
